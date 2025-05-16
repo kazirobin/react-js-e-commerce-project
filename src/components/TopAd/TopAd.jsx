3f335d5be0e27267a2f2ee7744e8c193be1345bf
kazirobin <kazirobinking@gmail.com>
2025-05-16 15:06:44 +0600

    update

--- src/components/TopAd/TopAd.jsx
+++ src/components/TopAd/TopAd.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { BsEnvelope } from 'react-icons/bs';
+import { IoLocationOutline } from "react-icons/io5";
 
 const TopAd = () => {
   return (
-    <div>TopAd</div>
+    <>
+    <div>
+
+    <div className='flex justify-center i text-amber-950 text-center font-extrabold'>
+<IoLocationOutline/>  23/A Mark Street Road, Newyork City |<BsEnvelope/>info@zillystore.com
+    </div>
+
+    </div>
+    </>
   )
 }
 
