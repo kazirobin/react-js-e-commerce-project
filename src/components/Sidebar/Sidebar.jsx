44ec01e376bb93819fee48d21f3be3a76ce478c1
kazirobin <kazirobinking@gmail.com>
2025-05-23 11:05:25 +0600

    sidebar search

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,12 +3,12 @@ import SidebarLinks from "./SidebarLinks";
 
 const Sidebar = ({ handleSidebar }) => {
   return (
-    <div className="">
+    <div className="absolute bottom-0">
       <div className="flex">
-        <div className="bg-green-800 hidden sm:block h-157 w-20 -mt-38 text-xl">
+        <div className="bg-green-800 hidden sm:block w-20 text-xl">
           <SidebarIcons handleSidebar={handleSidebar} />
         </div>
-        <div className="sm:-mt-38 w-100 bg-white">
+        <div className="w-100 bg-white">
           <SidebarLinks />
         </div>
         <div className="flex-1 opacity-50"></div>
