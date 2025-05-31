b0a6381861358f202a4087701a8faef055d0f661
kazirobin <kazirobinking@gmail.com>
2025-05-31 19:32:51 +0600

    sidebar positioning

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,6 +9,7 @@ const Sidebar = ({ handleSidebar }) => {
           <SidebarIcons handleSidebar={handleSidebar} />
         </div>
         <div className="w-full mt-13 sm:mt-0 sm:w-95 bg-white">
+          
           <SidebarLinks />
         </div>
         <div className="flex-1 opacity-50"></div>
