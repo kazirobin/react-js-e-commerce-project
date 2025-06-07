86e2be1f0f4e0ba43e2c52cccbd9841d8d8cf010
kazirobin <kazirobinking@gmail.com>
2025-06-07 16:56:07 +0600

    menus

--- src/components/Sidebar/Menus.jsx
+++ src/components/Sidebar/Menus.jsx
@@ -1,34 +1,30 @@
-import { useState } from "react";
-import { TbSquareRoundedMinus, TbSquareRoundedPlus } from "react-icons/tb";
-import { Link } from "react-router";
+import { useState } from "react"
+import { TbSquareRoundedMinus, TbSquareRoundedPlus } from "react-icons/tb"
+import { Link } from "react-router"
 
-const menu = [
-  { menu: "Home", sub: ["Home 01", "Home 02", "Home 03"] },
-  { menu: "Pages", sub: ["Pages 01", "Pages 02", "Pages 03"] },
-];
-
+const menuItems=[
+  {menu:"Home",sub:[{menu:"Home 1",src:"home-1"},{menu:"Home 2",src:"home-2"}]},
+  {menu:"Pages",sub:[{menu:"Pages 1",src:"pages-1"},{menu:"Pages 2",src:"pages-2"}]}
+]
 const Menus = () => {
-  const [links, setLinks] = useState(false);
-  console.log(menu.map(items => {items.sub}))
+  const [openMenus,setOpenMenus] = useState({})
+  const toggleMenu = (index) => { 
+    setOpenMenus((prev) => ({ ...prev,[index]:!prev[index] }))
+   }
   return (
-    <div className="w-full">
-      <div className="" onClick={() => {setLinks(!links);}}>
-        <div className="">
-          <div className="flex justify-between items-center">
-            <h6>Home</h6>
-          {links ? <TbSquareRoundedPlus /> : <TbSquareRoundedMinus />}
-          </div>
-          {links
-            ? ""
-            : menu.map(items => { items.sub.map(item => (item)) })}
-        </div>
-        <div className="">
-          <Link>Home 01</Link>
-          <Link>Home 02</Link>
-          <Link>Home 03</Link>
-        </div>
-      </div>
+    <div className="w-full"> 
+  {menuItems.map((items,index) =>  (<div key={index}>
+    <div className="flex justify-between items-center cursor-pointer" onClick={() => { toggleMenu(index) }}>
+    <h1>{items.menu }</h1>
+    {openMenus[index] ? <TbSquareRoundedMinus/> : <TbSquareRoundedPlus/>}
     </div>
-  );
-};
-export default Menus;
+    {openMenus[index] && (<div className="">
+      {items.sub.map((subItem,subItemIndex) => (<div key={subItemIndex}>
+        <Link to={subItem.src}>{subItem.menu}</Link>
+      </div>))}
+    </div>)}
+  </div>) )}
+    </div>
+  )
+}
+export default Menus
--- src/components/Sidebar/README.md
+++ src/components/Sidebar/README.md
@@ -23,3 +23,44 @@ navigation link
 info@zillystore.com
 +9888-256-666
 
+
+
+### Explain menus
+import { useState } from "react";
+import { TbSquareRoundedMinus, TbSquareRoundedPlus } from "react-icons/tb";
+import { Link } from "react-router"; // Corrected import
+const menuItems = [
+  { menu: "Home", sub: ["Home 01", "Home 02", "Home 03"] },
+  { menu: "Pages", sub: ["Pages 01", "Pages 02", "Pages 03"] },
+];
+const Menus = () => {
+  const [openMenus, setOpenMenus] = useState({});
+  const toggleMenu = (index) => {
+    setOpenMenus((prev) => ({
+      ...prev,
+      [index]: !prev[index],}));
+  };
+  return (
+    <div className="w-full">
+      {menuItems.map((item, index) => (
+        <div key={index}>
+          <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleMenu(index)}>
+            <h1>{item.menu}</h1>
+            {openMenus[index] ? <TbSquareRoundedMinus /> : <TbSquareRoundedPlus />}
+          </div>
+          {openMenus[index] && (
+            <div className="">
+              {item.sub.map((subItem, subIndex) => (
+                <div key={subIndex}>
+                  <Link to={`/${subItem.toLowerCase().replace(/\s/g, "-")}`}>{subItem}</Link>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menus;
